a9b9691be92eb2a5f7109fc7d6ebc27e1495f3bc
Brooks, Keanu Lee <Keanu.B.Lee@gatech.edu>
2017-04-21 15:36:56 -0400

    Recipelist fix

--- app/src/main/java/edu/gatech/foodaggregate/Recipe_Item.java
+++ app/src/main/java/edu/gatech/foodaggregate/Recipe_Item.java
@@ -3,6 +3,7 @@ package edu.gatech.foodaggregate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,16 +11,22 @@ import android.widget.TextView;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.cognito.CognitoSyncManager;
 import com.amazonaws.mobileconnectors.cognito.Dataset;
+import com.amazonaws.mobileconnectors.cognito.DefaultSyncCallback;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+import com.amazonaws.services.dynamodbv2.model.*;
 import org.json.JSONArray;
+import org.w3c.dom.Text;
+
+import mobile.AWSMobileClient;
 
 /**
  * Created by Keanu on 4/17/2017.
@@ -38,28 +45,33 @@ public class Recipe_Item extends AppCompatActivity{
         setContentView(R.layout.recipe_item);
         fav_button = (Button)findViewById(R.id.favorite_button);
         CognitoUserPool userPool = new CognitoUserPool(getApplicationContext(), "us-east-1_ZPUPIylup", "72lqi02sjv18jsgom0722tmisa", "1opq49mtggjo0r2ul0jq6mtj2ej78ofph293pfb7kf53gvv7ee7f");
-         user = userPool.getCurrentUser();
-         credentialsProvider = new CognitoCachingCredentialsProvider(
+        user = userPool.getCurrentUser();
+        credentialsProvider = new CognitoCachingCredentialsProvider(
                 getApplicationContext(), // Context
                 "us-east-1:654d88ee-2726-41e0-960f-3be89f3530db", // Identity Pool ID
                 Regions.US_EAST_1); // Region
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
-  ///       client = new CognitoSyncManager(getApplicationContext(),Regions.US_EAST_1,credentialsProvider);
+        ///       client = new CognitoSyncManager(getApplicationContext(),Regions.US_EAST_1,credentialsProvider);
 //         dataset = client.openOrCreateDataset("Favorites");
         AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
         mapper = new DynamoDBMapper(ddbClient);
-       /* Runnable runnable = new Runnable() {
+        Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                    Favorites favorites = new Favorites();
-                    favorites.setRecipeID(id);
-                    favorites.setUserName(user.getUserId());
-                    mapper.save(favorites);
+                try {
+                    Favorites favorites = mapper.load(Favorites.class, user.getUserId());
+                    String data = favorites.getRecipeID();
+                    if (data.contains(id)) {
+                        fav_button.setText("Unfavorite");
+                    }
+                } catch(Exception e) {
+
+                }
             }
         };
         Thread mythread = new Thread(runnable);
-        mythread.start();*/
+        mythread.start();
 
 
         TextView title = (TextView) findViewById(R.id.title);
@@ -80,7 +92,7 @@ public class Recipe_Item extends AppCompatActivity{
                 ingredient_list = ingredient_list + ingredientsArr.getJSONObject(i).get("amount").toString() + " " + ingredientsArr.getJSONObject(i).get("unit");
                 ingredient_list = ingredient_list + "\n";
             }
-                    ingredients.setText(ingredient_list);
+            ingredients.setText(ingredient_list);
 
             String directions_raw = response.getBody().getObject().get("instructions").toString();
             String directions_edit = directions_raw.replace(".","\n\n");
@@ -127,6 +139,8 @@ public class Recipe_Item extends AppCompatActivity{
                                     data = data.replace(id + ",", "");
                                 } else if (data.contains("," + id)) {
                                     data = data.replace("," + id, "");
+                                } else if (data.contains(id)) {
+                                    data = data.replace(id,"");
                                 }
                                 fav_button.setText("Favorite");
                             }
@@ -144,7 +158,7 @@ public class Recipe_Item extends AppCompatActivity{
             }
         };
         mythread.start();
-      //  TextView text = (TextView) findViewById(R.id.title);
+        //  TextView text = (TextView) findViewById(R.id.title);
        /* if (fav_button.getText().equals("Favorite")) {
             if (data.isEmpty()) {
                 dataset.put("RecipeIDs", id);
@@ -167,10 +181,10 @@ public class Recipe_Item extends AppCompatActivity{
             }
             dataset.put("RecipeIDs",data);
          *///   fav_button.setText("Favorite");
- //       }
-     //   text.setText(data);
+        //       }
+        //   text.setText(data);
 
-       // dataset.synchronize(new DefaultSyncCallback());
+        // dataset.synchronize(new DefaultSyncCallback());
 
 
     }
